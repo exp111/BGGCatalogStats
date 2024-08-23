1ff2cc1fae7f76e860d8d084fead0bff00e14fda
Exp <17885980+exp111@users.noreply.github.com>
2024-08-23 11:27:06 +0200

    rename

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'BGGCollection' title`, () => {
+  it(`should have the 'BGGCollectionStats' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BGGCollection');
+    expect(app.title).toEqual('BGGCollectionStats');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BGGCollection');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BGGCollectionStats');
   });
 });
