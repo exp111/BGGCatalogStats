ee5be434958550b7d372d24bfc4c5e37062dcb80
Exp <17885980+exp111@users.noreply.github.com>
2024-09-07 15:12:53 +0200

    nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MarvelchampionsComponent} from "./marvelchampions/marvelchampions.component";
+import {StartComponent} from "./start/start.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "marvelchampions", component: MarvelchampionsComponent, title: MarvelchampionsComponent.Title},
+  {path: "**", component: StartComponent, title: StartComponent.Title},
+];
