0151afbb1a9b6d3e29c4ecd4cad7642c1a631c4a
Exp <17885980+exp111@users.noreply.github.com>
2024-09-19 11:31:27 +0200

    navbar always

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {Routes} from '@angular/router';
 import {MarvelChampionsComponent} from "./game/marvel-champions/marvel-champions.component";
 import {StartComponent} from "./start/start.component";
 import {TooManyBonesComponent} from "./game/too-many-bones/too-many-bones.component";
+import {HelpPageComponent} from "./help-page/help-page.component";
 
 export const routes: Routes = [
   {path: "marvelchampions", component: MarvelChampionsComponent, title: MarvelChampionsComponent.Title},
   {path: "toomanybones", component: TooManyBonesComponent, title: TooManyBonesComponent.Title},
+  {path: "help", component: HelpPageComponent, title: HelpPageComponent.Title},
   {path: "**", component: StartComponent, title: StartComponent.Title},
 ];
